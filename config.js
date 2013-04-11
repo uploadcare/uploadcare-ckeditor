ef713a0685bb41f90eb8b5ae6ccd0bf3ebd1b632
Gray Hound <admin@devilmaydie.name>
2013-04-11 14:14:02 +0400

    USE_PHP config variable - defines if PHP should be used. PHP dialog is outdated now. New uploadcare widget with crop dialog.

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
 var UPLOADCARE_PUBLIC_KEY = "demopublickey";
+/*PHP Interface is outdated. By setting USE_PHP to false you will force plugin to autostore and use crop dialog */
+var USE_PHP = true;
--- plugin.js
+++ plugin.js
@@ -1,24 +1,53 @@
+UPLOADCARE_CROP = true;
+
 CKEDITOR.plugins.add('uploadcare', {
     requires : [ 'iframedialog' ],
     init : function(editor) {
         var me = this;
         var _file_id;
 
-        CKEDITOR.scriptLoader.load('https://ucarecdn.com/widget/0.6.8/uploadcare/uploadcare-0.6.8.min.js');
-        CKEDITOR.scriptLoader.load(me.path + 'config.js');
+        CKEDITOR.scriptLoader.load('https://ucarecdn.com/widget/0.6.9.2/uploadcare/uploadcare-0.6.9.2.min.js');
+        CKEDITOR.scriptLoader.load(me.path + 'config.js', function() {
+            UPLOADCARE_CROP = !USE_PHP;
+        });
 
         editor.addCommand('uploadcareDialog', new CKEDITOR.dialogCommand('uploadcareDialog'));
 
-        editor.addCommand('showUploadcareDialog', {exec: function() {
-            var circle = new uploadcare.Circle('.cke_button__uploadcare_icon');
-            var dialog = uploadcare.openDialog().done(function(file) {
-                circle.listen(file);
-                file.done(function(fileInfo) {
-                    _file_id = fileInfo.uuid;
-                    editor.execCommand('uploadcareDialog', true);
+        editor.addCommand('showUploadcareDialog', {
+            exec : function() {
+                var circle = new uploadcare.Circle('.cke_button__uploadcare_icon');
+                var dialog = uploadcare.openDialog().done(function(file) {
+                    circle.listen(file);
+                    file.done(function(fileInfo) {
+                        _file_id = fileInfo.uuid;
+                        dialog_path = me.path + 'dialog.php?file_id=' + _file_id;
+                        url = fileInfo.cdnUrl;
+                        if (USE_PHP) {
+                            editor.execCommand('uploadcareDialog', true);
+                        } else {
+                            if (window.XMLHttpRequest) {
+                                xmlHttpRequst = new XMLHttpRequest();
+                            } else if (window.ActiveXObject) {
+                                xmlHttpRequst = new ActiveXObject("Microsoft.XMLHTTP");
+                            }
+
+                            if (xmlHttpRequst != false) {
+                                xmlHttpRequst.open('GET', dialog_path, true);
+                                xmlHttpRequst.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                                xmlHttpRequst.onreadystatechange = function() {
+                                    if (xmlHttpRequst.readyState == 4) {
+                                        CKEDITOR.instances[editor.name].insertHtml('<img src="'+url+'" />');
+                                    }
+                                }
+                                xmlHttpRequst.send();
+                            } else {
+                                alert("Please use browser with Ajax support.!");
+                            }
+                        }
+                    });
                 });
-            });
-        }});
+            }
+        });
 
         editor.ui.addButton('Uploadcare', {
             label : 'Uploadcare',
@@ -41,12 +70,14 @@ CKEDITOR.plugins.add('uploadcare', {
                     expand : false,
                     elements : [ {
                         type : 'iframe',
-                        src : me.path + 'dialog.php?file_id='+_file_id,
+                        src : me.path + 'dialog.php?file_id=' + _file_id,
                         width : '800',
                         height : '600',
                         onContentLoad : function() {
                             var iframe = document.getElementById(this._.frameId), iframeWindow = iframe.contentWindow;
-                            iframeWindow.document.getElementById('editor_name').value = editor.name;
+                            if (iframeWindow.document.getElementById('editor_name')) {
+                                iframeWindow.document.getElementById('editor_name').value = editor.name;
+                            }
                         }
                     } ]
                 } ]
