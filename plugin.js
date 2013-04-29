078b9286d4e08c2fc1744206154d6914618b214a
Gray Hound <admin@devilmaydie.name>
2013-04-29 15:36:51 +0400

    New uploadcare widget version 0.8.1.2

--- plugin.js
+++ plugin.js
@@ -7,7 +7,7 @@ CKEDITOR.plugins.add('uploadcare', {
 
         UPLOADCARE_CROP = !editor.config.USE_PHP;
         UPLOADCARE_AUTOSTORE = !editor.config.USE_PHP;
-        CKEDITOR.scriptLoader.load('https://ucarecdn.com/widget/0.8.1/uploadcare/uploadcare-0.8.1.min.js');        
+        CKEDITOR.scriptLoader.load('https://ucarecdn.com/widget/0.8.1.2/uploadcare/uploadcare-0.8.1.2.min.js');        
 
         editor.addCommand('uploadcareDialog', new CKEDITOR.dialogCommand('uploadcareDialog'));
 
