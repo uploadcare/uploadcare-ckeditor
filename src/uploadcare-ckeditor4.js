4cd37730bad9855e7aae3daec5ea12f1193adc50
Александр Гренишин <nd0ut.me@gmail.com>
2018-04-27 17:41:27 +0300

    add eslint

--- src/uploadcare-ckeditor4.js
+++ src/uploadcare-ckeditor4.js
@@ -1,22 +1,22 @@
 CKEDITOR.plugins.add('uploadcare', {
   hidpi: true,
   icons: 'uploadcare',
-  init : function(editor) {
+  init: function(editor) {
     var config = editor.config.uploadcare || {};
 
     // Check if Uploadcare is already loaded and load it if not.
     if (typeof uploadcare === 'undefined') {
-        var version = config.widgetVersion || '$_WIDGET_VERSION';
-        var widget_url = 'https://ucarecdn.com/libs/widget/' + version +
-                 '/uploadcare.full.min.js'
-        CKEDITOR.scriptLoader.load(widget_url);
+      var version = config.widgetVersion || '$_WIDGET_VERSION';
+      var widgetUrl = 'https://ucarecdn.com/libs/widget/' + version + '/uploadcare.full.min.js';
+
+      CKEDITOR.scriptLoader.load(widgetUrl);
     }
 
     // Apply default properties.
-    if ( ! ('crop' in config)) {
+    if (!('crop' in config)) {
       config.crop = '';
     }
-    
+
     applyIntegrationSetting();
 
     function applyIntegrationSetting() {
@@ -31,11 +31,13 @@ CKEDITOR.plugins.add('uploadcare', {
     function searchSelectedElement(editor, needle) {
       var sel = editor.getSelection();
       var element = sel.getSelectedElement();
+
       if (element && element.is(needle)) {
         return element;
       }
 
       var widget;
+
       if (editor.widgets && (widget = editor.widgets.selected[0])) {
         if (widget.element.is(needle)) {
           return widget.element;
@@ -43,8 +45,10 @@ CKEDITOR.plugins.add('uploadcare', {
       }
 
       var range = sel.getRanges()[0];
+
       if (range) {
         range.shrink(CKEDITOR.SHRINK_TEXT);
+
         return editor.elementPath(range.getCommonAncestor()).contains(needle, 1);
       }
     }
@@ -52,57 +56,63 @@ CKEDITOR.plugins.add('uploadcare', {
     editor.addCommand('showUploadcareDialog', {
       allowedContent: 'img[!src,alt]{width,height};a[!href]',
       requiredContent: 'img[src];a[href]',
-      exec : function() {
+      exec: function() {
         if (typeof uploadcare == 'undefined') {
-          return; // not loaded yet
+          // not loaded yet
+          return;
         }
 
         uploadcare.plugin(function(uc) {
-          var settings, element, file;
+          var element;
+          var file;
+          var settings;
 
-          if (element = searchSelectedElement(editor, 'img')) {
+          if ((element = searchSelectedElement(editor, 'img'))) {
             file = element.getAttribute('src');
-          } else if (element = searchSelectedElement(editor, 'a')) {
+          }
+          else if ((element = searchSelectedElement(editor, 'a'))) {
             file = element.getAttribute('href');
           }
 
           if (file && uc.utils.splitCdnUrl(file)) {
-            settings = uc.settings.build(
-              uc.jQuery.extend({}, config, {multiple: false})
-            );
+            settings = uc.settings.build(uc.jQuery.extend({}, config, {multiple: false}));
             file = uploadcare.fileFrom('uploaded', file, settings);
-          } else {
-            settings = uc.settings.build(config)
+          }
+          else {
+            settings = uc.settings.build(config);
             file = null;
           }
 
-          var dialog = uploadcare.openDialog(file, settings).done(function(selected) {
+          uploadcare.openDialog(file, settings).done(function(selected) {
             var files = settings.multiple ? selected.files() : [selected];
+
             uc.jQuery.when.apply(null, files).done(function() {
               uc.jQuery.each(arguments, function() {
                 var imageUrl = this.cdnUrl;
-                if (this.isImage && ! this.cdnUrlModifiers) {
+
+                if (this.isImage && !this.cdnUrlModifiers) {
                   imageUrl += '-/preview/';
                 }
                 if (element) {
                   var widget;
-                  if (editor.widgets && (widget = editor.widgets.selected[0])
-                      && widget.element === element
-                  ) {
-                    widget.setData('src', imageUrl).setData('height', null)
-                  } else if (element.getName() == 'img') {
+
+                  if (editor.widgets && (widget = editor.widgets.selected[0]) && widget.element === element) {
+                    widget.setData('src', imageUrl).setData('height', null);
+                  }
+                  else if (element.getName() == 'img') {
                     element.data('cke-saved-src', '');
                     element.setAttribute('src', imageUrl);
-                  } else {
+                  }
+                  else {
                     element.data('cke-saved-href', '');
                     element.setAttribute('href', this.cdnUrl);
                   }
-                } else {
-                  if (this.isImage) {
-                    editor.insertHtml('<img src="' + imageUrl + '" alt="" /><br>', 'unfiltered_html');
-                  } else {
-                    editor.insertHtml('<a href="' + this.cdnUrl + '">' + this.name + '</a> ', 'unfiltered_html');
-                  }
+                }
+                else if (this.isImage) {
+                  editor.insertHtml('<img src="' + imageUrl + '" alt="" /><br>', 'unfiltered_html');
+                }
+                else {
+                  editor.insertHtml('<a href="' + this.cdnUrl + '">' + this.name + '</a> ', 'unfiltered_html');
                 }
               });
             });
@@ -111,10 +121,11 @@ CKEDITOR.plugins.add('uploadcare', {
       }
     });
 
-    editor.ui.addButton && editor.ui.addButton('Uploadcare', {
-      label : 'Uploadcare',
-      toolbar : 'insert',
-      command : 'showUploadcareDialog'
-    });
+    editor.ui.addButton &&
+      editor.ui.addButton('Uploadcare', {
+        label: 'Uploadcare',
+        toolbar: 'insert',
+        command: 'showUploadcareDialog'
+      });
   }
 });
