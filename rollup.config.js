2bb3a00d9fe7864a000f990064f9517fff206071
Александр Гренишин <nd0ut.me@gmail.com>
2018-04-27 23:01:49 +0300

    move widgetVersion to package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,28 @@
-import jscc from "rollup-plugin-jscc";
-import license from "rollup-plugin-license";
-import cp from "rollup-plugin-cp";
-
-const widgetVersion = "3.x";
+import jscc from 'rollup-plugin-jscc'
+import license from 'rollup-plugin-license'
+import cp from 'rollup-plugin-cp'
+import pkg from './package.json'
 
 export default {
-  input: "src/uploadcare-ckeditor4.js",
+  input: 'src/uploadcare-ckeditor4.js',
   plugins: [
     cp({
-      "icons": "dist/uploadcare/icons/"
+      icons: 'dist/uploadcare/icons/',
     }),
     license({
       banner: `
         Uploadcare CKEditor v4 plugin
         Version: <%= pkg.version %>
-      `
+      `,
     }),
     jscc({
       values: {
-        _WIDGET_VERSION: widgetVersion
-      }
-    })
+        _WIDGET_VERSION: pkg.widgetVersion,
+      },
+    }),
   ],
   output: {
-    file: "dist/uploadcare/plugin.js",
-    format: "iife"
-  }
-};
+    file: 'dist/uploadcare/plugin.js',
+    format: 'iife',
+  },
+}
