bab592e8c0dee51144d9a4b428780e7d46016b38
nd0ut <nd0ut.me@gmail.com>
2020-05-21 13:01:23 +0300

    fix assets copying

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,17 @@
 import jscc from 'rollup-plugin-jscc'
 import license from 'rollup-plugin-license'
-import cp from 'rollup-plugin-cp'
+import copy from 'rollup-plugin-copy'
 import pkg from './package.json'
 
 export default {
   input: 'src/uploadcare-ckeditor4.js',
   plugins: [
-    cp({'src/icons': 'dist/uploadcare/icons/'}),
+    copy({
+      targets: [{
+        src: 'src/icons',
+        dest: 'dist/uploadcare/',
+      }],
+    }),
     license({
       banner: `
         <%= pkg.name %> <%= pkg.version %>
